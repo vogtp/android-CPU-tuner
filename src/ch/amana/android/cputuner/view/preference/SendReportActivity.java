11e99f661778b026fcdc9e63c094f4d54882421d
Patrick S. Vogt <patrick.vogt@unibas.ch>
2011-08-02 22:40:39 +0200

    verbose output and less depth for battery

--- src/ch/amana/android/cputuner/view/preference/SendReportActivity.java
+++ src/ch/amana/android/cputuner/view/preference/SendReportActivity.java
@@ -79,7 +79,7 @@ public class SendReportActivity extends Activity {
 	}
 
 	private void sendBugReport() {
-
+		Logger.v("Send report: START");
 		String mailSubject = etSubject.getText().toString();
 		String mailBody = etMailBody.getText().toString();
 
@@ -102,48 +102,75 @@ public class SendReportActivity extends Activity {
 			return;
 		}
 
+		Logger.v("Send report: getting paths");
 		File path = new File(Environment.getExternalStorageDirectory(), getPackageName());
 		File file = new File(path, "report.zip");
 
 		Intent sendIntent = new Intent(Intent.ACTION_SEND);
 
+		Logger.v("Send report: getDeviceInfo");
 		getDeviceInfo();
+		Logger.v("Send report: getKernelInfo");
 		getKernelInfo();
+		Logger.v("Send report: CpuHandler.getInstance");
 		CpuHandler cpuHandler = CpuHandler.getInstance();
 
 		// sendIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]
 		// { "cputuner-help@lists.sourceforge.net" });
 		sendIntent.putExtra(Intent.EXTRA_SUBJECT, "cpu tuner report: " + mailSubject);
+		Logger.v("Send report: appending custom text");
 		StringBuilder body = new StringBuilder();
 		body.append(mailBody).append("\n\n");
 		body.append('\n').append("------------------------------------------").append('\n');
+		Logger.v("Send report: getting LogFile");
 		openLogFile(FILE_DEVICE_INFO);
+		Logger.v("Send report: getting Android release");
 		body.append("Android release: ").append(DeviceInformation.getAndroidRelease()).append('\n');
+		Logger.v("Send report: getting Device model");
 		body.append("Device model: ").append(DeviceInformation.getDeviceModel()).append('\n');
+		Logger.v("Send report: getting Manufacturer");
 		body.append("Manufacturer: ").append(DeviceInformation.getManufacturer()).append('\n');
-		body.append("Mod version: ").append(DeviceInformation.getModVersion()).append('\n');
-		body.append("Developer ID: ").append(DeviceInformation.getRomManagerDeveloperId()).append('\n');
-		body.append("Device nickname: ").append(DeviceInformation.getDeviceNick()).append('\n');
+		Logger.v("Send report: getting Mod version");
+		body.append("Mod version: Send report").append(DeviceInformation.getModVersion()).append('\n');
+		Logger.v("Send report: getting Developer ID");
+		body.append("Developer ID: Send report").append(DeviceInformation.getRomManagerDeveloperId()).append('\n');
+		Logger.v("Send report: getting Device nickname");
+		body.append("Device nickname: Send report").append(DeviceInformation.getDeviceNick()).append('\n');
+		Logger.v("Send report: getting PU tuner version");
 		body.append('\n').append("------------------------------------------").append('\n');
 		body.append("CPU tuner version: ").append(getString(R.string.version)).append('\n');
+		Logger.v("Send report: getting Language");
 		body.append("Language: ").append(Locale.getDefault().getLanguage()).append('\n');
-		body.append("Userlevel: ").append(SettingsStorage.getInstance().getUserLevel()).append('\n');
+		Logger.v("Send report: getting Userlevel");
+		body.append("Userlevel: Send report").append(SettingsStorage.getInstance().getUserLevel()).append('\n');
+		Logger.v("Send report: getting Beta mode");
 		body.append("Beta mode: ").append(SettingsStorage.getInstance().isEnableBeta()).append('\n');
+		Logger.v("Send report: getting Multicore");
 		body.append("Multicore: ").append(cpuHandler.getClass().getName()).append('\n');
+		Logger.v("Send report: getting system app");
 		body.append("Installed as system app: ").append(RootHandler.isSystemApp(this)).append('\n');
 		body.append('\n').append("------------------------------------------").append('\n');
+		Logger.v("Send report: getting CPU governors");
 		body.append("CPU governors: ").append(Arrays.toString(cpuHandler.getAvailCpuGov())).append('\n');
+		Logger.v("Send report: getting CPU frequencies");
 		body.append("CPU frequencies: ").append(Arrays.toString(cpuHandler.getAvailCpuFreq(true)));
 		if (!cpuHandler.hasAvailCpuFreq()) {
 			body.append(" (no available frequencies)");
 		}
 		body.append('\n');
+		Logger.v("Send report: getting Min scaling frequency");
 		body.append("Min scaling frequency: ").append(cpuHandler.getMinCpuFreq()).append('\n');
+		Logger.v("Send report: getting Max scaling frequency");
 		body.append("Max scaling frequency: ").append(cpuHandler.getMaxCpuFreq()).append('\n');
+		Logger.v("Send report: getting Current governor");
 		body.append("Current governor: ").append(cpuHandler.getCurCpuGov()).append('\n');
+		Logger.v("Send report: getting Current frequency");
 		body.append("Current frequency: ").append(cpuHandler.getCurCpuFreq()).append('\n');
+		Logger.v("Send report: getting BatteryHandler.getInstance()");
 		BatteryHandler batteryHandler = BatteryHandler.getInstance();
+		Logger.v("Send report: getting Current power usage");
 		body.append("Current power usage: ").append(batteryHandler.getBatteryCurrentNow()).append('\n');
+		Logger.v("Send report: getting Average power usage");
 		body.append("Average power usage: ").append(batteryHandler.getBatteryCurrentAverage()).append('\n');
 		closeLogFile();
 		body.append('\n').append("------------------------------------------").append('\n');
@@ -152,6 +179,7 @@ public class SendReportActivity extends Activity {
 		try {
 			DB.OpenHelper oh = new OpenHelper(this);
 			DataExporter dm = new DataJsonExporter(oh.getWritableDatabase(), new File(path, DIR_REPORT));
+			Logger.v("Send report: exporting DB");
 			try {
 				dm.export(DB.DATABASE_NAME);
 			} catch (Exception e) {
@@ -165,22 +193,31 @@ public class SendReportActivity extends Activity {
 		sendIntent.putExtra(Intent.EXTRA_TEXT, body.toString());
 
 		try {
+			Logger.v("Send report: appending file FILE_DEVICE_INFO");
 			ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(file));
 			addFileToZip(zip, "output", FILE_DEVICE_INFO);
+			Logger.v("Send report: appending file FILE_CAPABILITIESCHECK");
 			addFileToZip(zip, "", CapabilityCheckerActivity.FILE_CAPABILITIESCHECK);
+			Logger.v("Send report: appending file FILE_GETPROP");
 			addFileToZip(zip, "", FILE_GETPROP);
+			Logger.v("Send report: appending file FILE_KERNEL_CPUFREQ_CONFIG");
 			addFileToZip(zip, "", FILE_KERNEL_CPUFREQ_CONFIG);
+			Logger.v("Send report: appending file DB json");
 			addFileToZip(zip, "DB", DB.DATABASE_NAME + ".json");
+			Logger.v("Send report: appending file cpufreq");
 			addDirectoryToZip(zip, "cpufreq", new File(CpuHandler.CPU_BASE_DIR), 5);
-			addDirectoryToZip(zip, "battery", new File(BatteryHandler.BATTERY_DIR), 5);
+			Logger.v("Send report: appending file battery");
+			addDirectoryToZip(zip, "battery", new File(BatteryHandler.BATTERY_DIR), 1);
 			zip.flush();
 			zip.close();
 
 			Uri uri = Uri.fromFile(file);
 			sendIntent.putExtra(Intent.EXTRA_STREAM, uri);
 			sendIntent.setType("application/zip");
+			Logger.v("Send report: sending");
 			startActivity(sendIntent);
 			finish();
+			Logger.v("Send report: FINISHED");
 		} catch (IOException e) {
 			Logger.w("Error zipping attachments", e);
 		}
@@ -214,6 +251,9 @@ public class SendReportActivity extends Activity {
 	}
 
 	private void addFileToZip(ZipOutputStream zip, String zipDir, File file) {
+		if (file == null || !file.exists()) {
+			return;
+		}
 		try {
 			zip.putNextEntry(new ZipEntry(zipDir + "/" + file.getName()));
 			FileInputStream in = new FileInputStream(file);
@@ -226,7 +266,7 @@ public class SendReportActivity extends Activity {
 			zip.closeEntry();
 			in.close();
 		} catch (IOException e) {
-			Logger.w("Error exporting adding file to zip", e);
+			Logger.w("Error exporting adding file to zip " + file.getAbsolutePath(), e);
 		}
 
 	}
