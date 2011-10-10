0e57a833601717df18338a78db6a0f852d96dd6b
Patrick S. Vogt <patrick.vogt@unibas.ch>
2011-10-10 20:33:47 +0200

    display services as icons

--- src/ch/amana/android/cputuner/view/fragments/ProfilesListFragment.java
+++ src/ch/amana/android/cputuner/view/fragments/ProfilesListFragment.java
@@ -25,6 +25,7 @@ import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.SimpleCursorAdapter.ViewBinder;
@@ -50,6 +51,10 @@ import com.markupartist.android.widget.ActionBar.Action;
 
 public class ProfilesListFragment extends PagerListFragment {
 
+	private static final int ALPHA_ON = 200;
+	private static final int ALPHA_OFF = 40;
+	private static final int ALPHA_LEAVE = 100;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -68,8 +73,8 @@ public class ProfilesListFragment extends PagerListFragment {
 						DB.CpuProfile.NAME_FREQUENCY_MIN, DB.CpuProfile.NAME_FREQUENCY_MAX, DB.CpuProfile.NAME_WIFI_STATE, DB.CpuProfile.NAME_GPS_STATE,
 						DB.CpuProfile.NAME_BLUETOOTH_STATE, DB.CpuProfile.NAME_MOBILEDATA_3G_STATE, DB.CpuProfile.NAME_BACKGROUND_SYNC_STATE,
 						DB.CpuProfile.NAME_MOBILEDATA_CONNECTION_STATE, DB.CpuProfile.NAME_AIRPLANEMODE_STATE },
-				new int[] { R.id.tvName, R.id.tvGov, R.id.tvFreqMin, R.id.tvFreqMax, R.id.tvWifi, R.id.tvGPS, R.id.tvBluetooth,
-						R.id.tvMobiledata3G, R.id.tvSync, R.id.tvMobiledataConnection, R.id.tvAirplaneMode });
+				new int[] { R.id.tvName, R.id.tvGov, R.id.tvFreqMin, R.id.tvFreqMax, R.id.ivServiceWifi, R.id.ivServiceGPS, R.id.ivServiceBluetooth,
+						R.id.ivServiceMD3g, R.id.ivServiceSync, R.id.ivServiceMDCon, R.id.ivServiceAirplane });
 
 		adapter.setViewBinder(new ViewBinder() {
 			@Override
@@ -122,185 +127,96 @@ public class ProfilesListFragment extends PagerListFragment {
 					}
 					return true;
 				} else if (columnIndex == DB.CpuProfile.INDEX_GPS_STATE) {
-					TextView textView = (TextView) view;
-					if (!SettingsStorage.getInstance().isEnableSwitchGps()) {
-						textView.setText("");
-						return true;
+					if (SettingsStorage.getInstance().isEnableSwitchGps()) {
+						view.setVisibility(View.VISIBLE);
+						setServiceStateIcon((ImageView) view, cursor.getInt(columnIndex));
+					} else {
+						view.setVisibility(View.GONE);
 					}
-					int state = cursor.getInt(columnIndex);
-					int color = Color.DKGRAY;
-					int textRes = R.string.labelGpsOn;
-					if (state == PowerProfiles.SERVICE_STATE_ON) {
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_OFF) {
-						textRes = R.string.labelGpsOff;
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_PREV) {
-						textRes = R.string.labelGpsPrev;
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_PULSE) {
-
-						setPulseAnimation(textView);
-						color = Color.WHITE;
-					}
-					textView.setText(textRes);
-					textView.setTextColor(color);
 					return true;
 				} else if (columnIndex == DB.CpuProfile.INDEX_WIFI_STATE) {
-					TextView textView = (TextView) view;
-					if (!SettingsStorage.getInstance().isEnableSwitchWifi()) {
-						textView.setText("");
-						return true;
+					if (SettingsStorage.getInstance().isEnableSwitchWifi()) {
+						view.setVisibility(View.VISIBLE);
+						setServiceStateIcon((ImageView) view, cursor.getInt(columnIndex));
+					} else {
+						view.setVisibility(View.GONE);
 					}
-					int state = cursor.getInt(columnIndex);
-					int color = Color.DKGRAY;
-					int textRes = R.string.labelWifiOn;
-					if (state == PowerProfiles.SERVICE_STATE_ON) {
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_OFF) {
-						textRes = R.string.labelWifiOff;
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_PREV) {
-						textRes = R.string.labelWifiPrev;
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_PULSE) {
-						setPulseAnimation(textView);
-						color = Color.WHITE;
-					}
-					textView.setText(textRes);
-					textView.setTextColor(color);
 					return true;
 				} else if (columnIndex == DB.CpuProfile.INDEX_BLUETOOTH_STATE) {
-					TextView textView = (TextView) view;
-					if (!SettingsStorage.getInstance().isEnableSwitchBluetooth()) {
-						textView.setText("");
-						return true;
+					if (SettingsStorage.getInstance().isEnableSwitchBluetooth()) {
+						view.setVisibility(View.VISIBLE);
+						setServiceStateIcon((ImageView) view, cursor.getInt(columnIndex));
+					} else {
+						view.setVisibility(View.GONE);
 					}
-					int state = cursor.getInt(columnIndex);
-					int color = Color.DKGRAY;
-					int textRes = R.string.labelBluetoothOn;
-					if (state == PowerProfiles.SERVICE_STATE_ON) {
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_OFF) {
-						textRes = R.string.labelBluetoothOff;
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_PREV) {
-						textRes = R.string.labelBluetoothPrev;
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_PULSE) {
-
-						setPulseAnimation(textView);
-						color = Color.WHITE;
-					}
-					textView.setText(textRes);
-					textView.setTextColor(color);
 					return true;
 				} else if (columnIndex == DB.CpuProfile.INDEX_BACKGROUND_SYNC_STATE) {
-					TextView textView = (TextView) view;
-					if (!SettingsStorage.getInstance().isEnableSwitchBackgroundSync()) {
-						textView.setText("");
-						return true;
+					if (SettingsStorage.getInstance().isEnableSwitchBackgroundSync()) {
+						view.setVisibility(View.VISIBLE);
+						setServiceStateIcon((ImageView) view, cursor.getInt(columnIndex));
+					} else {
+						view.setVisibility(View.GONE);
 					}
-					int state = cursor.getInt(columnIndex);
-					int color = Color.DKGRAY;
-					int textRes = R.string.labelSyncOn;
-					if (state == PowerProfiles.SERVICE_STATE_ON) {
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_OFF) {
-						textRes = R.string.labelSyncOff;
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_PREV) {
-						textRes = R.string.labelSyncPrev;
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_PULSE) {
-
-						setPulseAnimation(textView);
-						color = Color.WHITE;
-					}
-					textView.setText(textRes);
-					textView.setTextColor(color);
 					return true;
 				} else if (columnIndex == DB.CpuProfile.INDEX_MOBILEDATA_3G_STATE) {
-					TextView textView = (TextView) view;
 					if (!SettingsStorage.getInstance().isEnableSwitchMobiledata3G()) {
-						textView.setText("");
+						view.setVisibility(View.GONE);
 						return true;
 					}
+					view.setVisibility(View.VISIBLE);
+					ImageView icon = (ImageView) view;
+					icon.clearAnimation();
 					int state = cursor.getInt(columnIndex);
-					int color = Color.DKGRAY;
-					int textRes = R.string.label3g2g;
 					if (state == PowerProfiles.SERVICE_STATE_2G) {
-						color = Color.LTGRAY;
-						textRes = R.string.label2g;
+						icon.setImageResource(R.drawable.serviceicon_md_2g);
 					} else if (state == PowerProfiles.SERVICE_STATE_2G_3G) {
-						color = Color.LTGRAY;
-						textRes = R.string.label3g2g;
+						icon.setImageResource(R.drawable.serviceicon_md_2g3g);
 					} else if (state == PowerProfiles.SERVICE_STATE_3G) {
-						color = Color.LTGRAY;
-						textRes = R.string.label3g;
+						icon.setImageResource(R.drawable.serviceicon_md_3g);
 					} else if (state == PowerProfiles.SERVICE_STATE_PREV) {
-						textRes = R.string.label3g2gPrev;
-						color = Color.LTGRAY;
+						icon.setImageResource(R.drawable.serviceicon_md_2g3g);
+						setAnimation(icon, R.anim.back);
 					}
-					textView.setTextColor(color);
-					textView.setText(textRes);
+					if (state == PowerProfiles.SERVICE_STATE_LEAVE) {
+						icon.setAlpha(ALPHA_LEAVE);
+					} else {
+						icon.setAlpha(ALPHA_ON);
+					}
+
 					return true;
 				} else if (columnIndex == DB.CpuProfile.INDEX_MOBILEDATA_CONNECTION_STATE) {
-					TextView textView = (TextView) view;
-					if (!SettingsStorage.getInstance().isEnableSwitchMobiledataConnection()) {
-						textView.setText("");
-						return true;
+					if (SettingsStorage.getInstance().isEnableSwitchMobiledataConnection()) {
+						view.setVisibility(View.VISIBLE);
+						setServiceStateIcon((ImageView) view, cursor.getInt(columnIndex));
+					} else {
+						view.setVisibility(View.GONE);
 					}
-					int state = cursor.getInt(columnIndex);
-					int color = Color.DKGRAY;
-					int textRes = R.string.labelMobiledataOn;
-					if (state == PowerProfiles.SERVICE_STATE_ON) {
-						color = Color.LTGRAY;
-						textRes = R.string.labelMobiledataOn;
-					} else if (state == PowerProfiles.SERVICE_STATE_OFF) {
-						color = Color.LTGRAY;
-						textRes = R.string.labelMobiledataOff;
-					} else if (state == PowerProfiles.SERVICE_STATE_PREV) {
-						textRes = R.string.labelMobiledataPrev;
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_PULSE) {
-						textRes = R.string.labelMobiledataOn;
-
-						setPulseAnimation(textView);
-						color = Color.WHITE;
-					}
-					textView.setTextColor(color);
-					textView.setText(textRes);
 					return true;
 				} else if (columnIndex == DB.CpuProfile.INDEX_AIRPLANEMODE_STATE) {
-					TextView textView = (TextView) view;
-					if (!SettingsStorage.getInstance().isEnableAirplaneMode()) {
-						textView.setText("");
-						return true;
+					if (SettingsStorage.getInstance().isEnableAirplaneMode()) {
+						view.setVisibility(View.VISIBLE);
+						setServiceStateIcon((ImageView) view, cursor.getInt(columnIndex));
+					} else {
+						view.setVisibility(View.GONE);
 					}
-					int state = cursor.getInt(columnIndex);
-					int color = Color.DKGRAY;
-					int textRes = R.string.labelAirplaneModeOn;
-					if (state == PowerProfiles.SERVICE_STATE_ON) {
-						color = Color.LTGRAY;
-						textRes = R.string.labelAirplaneModeOn;
-					} else if (state == PowerProfiles.SERVICE_STATE_OFF) {
-						color = Color.LTGRAY;
-						textRes = R.string.labelAirplaneModeOff;
-					} else if (state == PowerProfiles.SERVICE_STATE_PREV) {
-						textRes = R.string.labelAirplaneModePrev;
-						color = Color.LTGRAY;
-					} else if (state == PowerProfiles.SERVICE_STATE_PULSE) {
-						textRes = R.string.labelAirplaneModeOn;
-
-						setPulseAnimation(textView);
-						color = Color.WHITE;
-					}
-					textView.setTextColor(color);
-					textView.setText(textRes);
 					return true;
 				}
 				return false;
+			}
+
+			private void setServiceStateIcon(ImageView icon, int state) {
+				icon.clearAnimation();
+				if (state == PowerProfiles.SERVICE_STATE_LEAVE) {
+					icon.setAlpha(ALPHA_LEAVE);
+				} else if (state == PowerProfiles.SERVICE_STATE_OFF) {
+					icon.setAlpha(ALPHA_OFF);
+				} else if (state == PowerProfiles.SERVICE_STATE_PREV) {
+					setAnimation(icon, R.anim.back);
+				} else if (state == PowerProfiles.SERVICE_STATE_PULSE) {
+					setAnimation(icon, R.anim.pluse);
+				} else {
+					icon.setAlpha(ALPHA_ON);
+				}
 			}
 
 		});
@@ -309,8 +225,8 @@ public class ProfilesListFragment extends PagerListFragment {
 		getListView().setOnCreateContextMenuListener(this);
 	}
 
-	private void setPulseAnimation(final TextView tv) {
-		final AnimationSet c = (AnimationSet) AnimationUtils.loadAnimation(getActivity(), R.anim.pluse);
+	private void setAnimation(final View v, int resID) {
+		final AnimationSet c = (AnimationSet) AnimationUtils.loadAnimation(getActivity(), resID);
 		c.setRepeatMode(Animation.RESTART);
 		c.setRepeatCount(Animation.INFINITE);
 		c.setAnimationListener(new AnimationListener() {
@@ -320,17 +236,18 @@ public class ProfilesListFragment extends PagerListFragment {
 
 			@Override
 			public void onAnimationRepeat(Animation animation) {
+				Logger.i("Repeat anim");
 			}
 
 			@Override
 			public void onAnimationEnd(Animation animation) {
-				tv.clearAnimation();
-				tv.startAnimation(c);
+				v.clearAnimation();
+				v.startAnimation(c);
 			}
 		});
 
-		tv.clearAnimation();
-		tv.startAnimation(c);
+		v.clearAnimation();
+		v.startAnimation(c);
 	}
 
 	@Override
