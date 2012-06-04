db4876121ac7224ce4761939354b3f80b0b175d5
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-06-04 20:57:36 +0200

    fix fc

--- src/ch/amana/android/cputuner/receiver/StatisticsReceiver.java
+++ src/ch/amana/android/cputuner/receiver/StatisticsReceiver.java
@@ -50,81 +50,84 @@ public class StatisticsReceiver extends BroadcastReceiver {
 	}
 
 	private static void updateStatistics(Context context) {
-		Logger.d("Adding timeinstate to input queue");
-		//		SettingsStorage settings = SettingsStorage.getInstance(context);
-		String timeinstate = CpuHandler.getInstance().getCpuTimeinstate();
-		TimeInStateParser tisParser = new TimeInStateParser(timeinstate);
-		String triggerName = triggerNameOld;
-		String profileName = profileNameOld;
-		String virtGovName = virtGovNameOld;
-		triggerNameOld = PowerProfiles.getInstance(context).getCurrentTriggerName();
-		profileNameOld = PowerProfiles.getInstance(context).getCurrentProfileName();
-		virtGovNameOld = PowerProfiles.getInstance(context).getCurrentVirtGovName();
+		try {
+			Logger.d("Adding timeinstate to input queue");
+			//		SettingsStorage settings = SettingsStorage.getInstance(context);
+			String timeinstate = CpuHandler.getInstance().getCpuTimeinstate();
+			TimeInStateParser tisParser = new TimeInStateParser(timeinstate);
+			String triggerName = triggerNameOld;
+			String profileName = profileNameOld;
+			String virtGovName = virtGovNameOld;
+			triggerNameOld = PowerProfiles.getInstance(context).getCurrentTriggerName();
+			profileNameOld = PowerProfiles.getInstance(context).getCurrentProfileName();
+			virtGovNameOld = PowerProfiles.getInstance(context).getCurrentVirtGovName();
 
+			if (oldTisParser != null && triggerName != null && profileName != null && virtGovName != null) {
+				ContentResolver resolver = context.getContentResolver();
 
-		if (oldTisParser != null && triggerName != null && profileName != null && virtGovName != null) {
-			ContentResolver resolver = context.getContentResolver();
+				Cursor indexCursor = resolver.query(TimeInStateIndex.CONTENT_URI, DB.PROJECTION_ID, TimeInStateIndex.SELECTION_TRIGGER_PROFILE_VIRTGOV, new String[] { triggerName,
+						profileName, virtGovName }, TimeInStateIndex.SORTORDER_DEFAULT);
 
-			Cursor indexCursor = resolver.query(TimeInStateIndex.CONTENT_URI, DB.PROJECTION_ID, TimeInStateIndex.SELECTION_TRIGGER_PROFILE_VIRTGOV, new String[] { triggerName,
-					profileName, virtGovName }, TimeInStateIndex.SORTORDER_DEFAULT);
-
-			long indexID = Long.MIN_VALUE;
-			if (indexCursor.moveToFirst()) {
-				indexID = indexCursor.getLong(DB.INDEX_ID);
-			} else {
-				ContentValues values = new ContentValues();
-				values.put(TimeInStateIndex.NAME_TRIGGER, triggerName);
-				values.put(TimeInStateIndex.NAME_PROFILE, profileName);
-				values.put(TimeInStateIndex.NAME_VIRTGOV, virtGovName);
-				Uri uri = resolver.insert(TimeInStateIndex.CONTENT_URI, values);
-				indexID = ContentUris.parseId(uri);
-			}
-			if (indexCursor != null) {
-				indexCursor.close();
-			}
-
-			tisParser.setBaseline(oldTisParser);
-
-			String idStr = Long.toString(indexID);
-			//			ArrayList<ContentProviderOperation> operations = new ArrayList<ContentProviderOperation>(tisParser.getStates().size());
-			//			Builder opp;
-			for (Integer state : tisParser.getStates()) {
-				Long time = tisParser.getTime(state);
-				String[] selection = new String[] { idStr, Integer.toString(state) };
-				Cursor c = resolver.query(TimeInStateValue.CONTENT_URI, TimeInStateValue.PROJECTION_DEFAULT, TimeInStateValue.SELECTION_BY_ID_STATE, selection,
-						TimeInStateValue.SORTORDER_DEFAULT);
-				ContentValues values = new ContentValues();
-				if (c.moveToFirst()) {
-					time += c.getLong(TimeInStateValue.INDEX_TIME);
-					values = new ContentValues();
-					values.put(TimeInStateValue.NAME_TIME, time);
-					resolver.update(TimeInStateValue.CONTENT_URI, values, TimeInStateValue.SELECTION_BY_ID_STATE, selection);
-					//					opp = ContentProviderOperation.newUpdate(TimeInStateValue.CONTENT_URI);
-					//					opp.withSelection(TimeInStateValue.SELECTION_BY_ID_STATE, selection);
-					//					opp.withValues(values);
+				long indexID = Long.MIN_VALUE;
+				if (indexCursor.moveToFirst()) {
+					indexID = indexCursor.getLong(DB.INDEX_ID);
 				} else {
-					values.put(TimeInStateValue.NAME_IDX, indexID);
-					values.put(TimeInStateValue.NAME_STATE, state);
-					values.put(TimeInStateValue.NAME_TIME, time);
-					resolver.insert(TimeInStateValue.CONTENT_URI, values);
-					//					opp = ContentProviderOperation.newUpdate(TimeInStateValue.CONTENT_URI);
-					//					opp.withValues(values);
+					ContentValues values = new ContentValues();
+					values.put(TimeInStateIndex.NAME_TRIGGER, triggerName);
+					values.put(TimeInStateIndex.NAME_PROFILE, profileName);
+					values.put(TimeInStateIndex.NAME_VIRTGOV, virtGovName);
+					Uri uri = resolver.insert(TimeInStateIndex.CONTENT_URI, values);
+					indexID = ContentUris.parseId(uri);
 				}
-				//				operations.add(opp.build());
-				if (c != null) {
-					c.close();
+				if (indexCursor != null) {
+					indexCursor.close();
 				}
+
+				tisParser.setBaseline(oldTisParser);
+
+				String idStr = Long.toString(indexID);
+				//			ArrayList<ContentProviderOperation> operations = new ArrayList<ContentProviderOperation>(tisParser.getStates().size());
+				//			Builder opp;
+				for (Integer state : tisParser.getStates()) {
+					Long time = tisParser.getTime(state);
+					String[] selection = new String[] { idStr, Integer.toString(state) };
+					Cursor c = resolver.query(TimeInStateValue.CONTENT_URI, TimeInStateValue.PROJECTION_DEFAULT, TimeInStateValue.SELECTION_BY_ID_STATE, selection,
+							TimeInStateValue.SORTORDER_DEFAULT);
+					ContentValues values = new ContentValues();
+					if (c.moveToFirst()) {
+						time += c.getLong(TimeInStateValue.INDEX_TIME);
+						values = new ContentValues();
+						values.put(TimeInStateValue.NAME_TIME, time);
+						resolver.update(TimeInStateValue.CONTENT_URI, values, TimeInStateValue.SELECTION_BY_ID_STATE, selection);
+						//					opp = ContentProviderOperation.newUpdate(TimeInStateValue.CONTENT_URI);
+						//					opp.withSelection(TimeInStateValue.SELECTION_BY_ID_STATE, selection);
+						//					opp.withValues(values);
+					} else {
+						values.put(TimeInStateValue.NAME_IDX, indexID);
+						values.put(TimeInStateValue.NAME_STATE, state);
+						values.put(TimeInStateValue.NAME_TIME, time);
+						resolver.insert(TimeInStateValue.CONTENT_URI, values);
+						//					opp = ContentProviderOperation.newUpdate(TimeInStateValue.CONTENT_URI);
+						//					opp.withValues(values);
+					}
+					//				operations.add(opp.build());
+					if (c != null) {
+						c.close();
+					}
+				}
+				//			try {
+				//				Logger.w("goint to update state statistics");
+				//				ContentProviderResult[] applyBatch = resolver.applyBatch(CpuTunerProvider.AUTHORITY, operations);
+				//				Logger.w("updated state statistics");
+				//			} catch (Exception e) {
+				//				Logger.w("Cannot save time in state statistics", e);
+				//			}
 			}
-			//			try {
-			//				Logger.w("goint to update state statistics");
-			//				ContentProviderResult[] applyBatch = resolver.applyBatch(CpuTunerProvider.AUTHORITY, operations);
-			//				Logger.w("updated state statistics");
-			//			} catch (Exception e) {
-			//				Logger.w("Cannot save time in state statistics", e);
-			//			}
+			tisParser.setBaseline(null);
+			oldTisParser = tisParser;
+		} catch (Exception e) {
+			Logger.i("Cannot save time in state information", e);
 		}
-		tisParser.setBaseline(null);
-		oldTisParser = tisParser;
 	}
 
 	public static void register(Context context) {
