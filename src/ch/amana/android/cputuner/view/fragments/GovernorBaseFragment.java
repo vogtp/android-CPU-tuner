5183012d8f2f672c478e85709249fdae34947e71
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-06-29 23:04:02 +0200

    fix save in autoload config

--- src/ch/amana/android/cputuner/view/fragments/GovernorBaseFragment.java
+++ src/ch/amana/android/cputuner/view/fragments/GovernorBaseFragment.java
@@ -1,5 +1,6 @@
 package ch.amana.android.cputuner.view.fragments;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import ch.amana.android.cputuner.model.IGovernorModel;
 
@@ -24,6 +25,91 @@ public abstract class GovernorBaseFragment extends Fragment {
 	}
 
 	public IGovernorModel getGovernorModel() {
+		if (governor == null) {
+			return new IGovernorModel() {
+
+				@Override
+				public void setVirtualGovernor(long id) {
+				}
+
+				@Override
+				public void setUseNumberOfCpus(int position) {
+				}
+
+				@Override
+				public void setScript(String string) {
+				}
+
+				@Override
+				public void setPowersaveBias(int powersaveBias) {
+				}
+
+				@Override
+				public void setGovernorThresholdUp(String string) {
+				}
+
+				@Override
+				public void setGovernorThresholdUp(int i) {
+				}
+
+				@Override
+				public void setGovernorThresholdDown(String string) {
+				}
+
+				@Override
+				public void setGovernorThresholdDown(int i) {
+				}
+
+				@Override
+				public void setGov(String gov) {
+				}
+
+				@Override
+				public boolean hasScript() {
+					return false;
+				}
+
+				@Override
+				public long getVirtualGovernor() {
+					return 0;
+				}
+
+				@Override
+				public int getUseNumberOfCpus() {
+					return 0;
+				}
+
+				@Override
+				public String getScript() {
+					return null;
+				}
+
+				@Override
+				public int getPowersaveBias() {
+					return 0;
+				}
+
+				@Override
+				public int getGovernorThresholdUp() {
+					return 0;
+				}
+
+				@Override
+				public int getGovernorThresholdDown() {
+					return 0;
+				}
+
+				@Override
+				public String getGov() {
+					return null;
+				}
+
+				@Override
+				public CharSequence getDescription(Context ctx) {
+					return null;
+				}
+			};
+		}
 		return governor;
 	}
 
