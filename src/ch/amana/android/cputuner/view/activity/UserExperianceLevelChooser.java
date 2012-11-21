38e1d1c73674fd3cca59770cb3a8ebb04053f61f
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-11-21 14:12:36 +0100

    choose userlevel by clicking on text too

--- src/ch/amana/android/cputuner/view/activity/UserExperianceLevelChooser.java
+++ src/ch/amana/android/cputuner/view/activity/UserExperianceLevelChooser.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import ch.amana.android.cputuner.R;
 import ch.amana.android.cputuner.helper.SettingsStorage;
 
@@ -29,6 +30,32 @@ public class UserExperianceLevelChooser extends Dialog {
 	    	userLevel = R.id.rbBeginner;
 	    }
 		rgUserLevel.check(userLevel);
+		
+		TextView tvNormal = (TextView)findViewById(R.id.tvNormal);
+		tvNormal.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				rgUserLevel.check(R.id.rbNormal);
+			}
+		});
+		TextView tvPowerUser = (TextView) findViewById(R.id.tvPowerUser);
+		tvPowerUser.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				rgUserLevel.check(R.id.rbPowerUser);
+			}
+		});
+		TextView tvBeginner = (TextView) findViewById(R.id.tvBeginner);
+		tvBeginner.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				rgUserLevel.check(R.id.rbBeginner);
+			}
+		});
+		
 		
 		((Button)findViewById(R.id.buOk)).setOnClickListener(new View.OnClickListener() {
 			
